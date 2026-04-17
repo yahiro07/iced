1527ff27c5af2a4f985a44e59bcd83437d662a1b
yahiro <yahiro07@users.noreply.github.com>
2026-04-17 09:43:00 +0900

    initial commit

---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+import { mountAppRoot } from "@/utils/mount-app-root";
+
+const App = () => {
+  return <div>hello</div>;
+};
+mountAppRoot(<App />, "app");
---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    tsconfigPaths: true,
+  },
+  server: {
+    port: 3000,
+  },
+});
