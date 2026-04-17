b4f67808aaa175402e42ceea9404188e4d2adc3d
yahiro <yahiro07@users.noreply.github.com>
2026-04-17 10:05:22 +0900

    add points

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,27 @@ import { seqNumbers } from "@/utils/array-utils";
 import { mountAppRoot } from "@/utils/mount-app-root";
 import "./styling/page.css";
 import "./styling/utility-classes.css";
+import { useCallback } from "react";
+import { createStore } from "snap-store";
+
+type Point = { x: number; y: number };
+
+const store = createStore<{ points: Point[] }>({
+  points: [],
+});
 
 const EditorArea = () => {
+  const { points } = store.useSnapshot();
+  const addPoint = useCallback((e: React.PointerEvent) => {
+    const el = e.currentTarget as SVGRectElement;
+    const rect = el.getBoundingClientRect();
+    const po: Point = {
+      x: e.clientX - rect.left - 200,
+      y: e.clientY - rect.top - 200,
+    };
+    store.setPoints((prev) => [...prev, po]);
+  }, []);
+
   return (
     <div
       css={{
@@ -17,6 +36,7 @@ const EditorArea = () => {
         height="400"
         viewBox="-200 -200 400 400"
         xmlns="http://www.w3.org/2000/svg"
+        onPointerDown={addPoint}
       >
         <g stroke="#ccc">
           {seqNumbers(10).map((xi) => {
@@ -27,6 +47,17 @@ const EditorArea = () => {
             const y = yi * 40 - 200;
             return <line key={y} x1="-200" y1={y} x2="200" y2={y} />;
           })}
+        </g>
+        <g>
+          {points.map((point) => (
+            <circle
+              key={`${point.x}-${point.y}`}
+              cx={point.x}
+              cy={point.y}
+              r={2}
+              fill="#a6f"
+            />
+          ))}
         </g>
       </svg>
     </div>
